4881e9acf57f6d876cbd33a1ab8c42ab7ad60a68
firebingo <matthew.t.kides@gmail.com>
2019-05-31 21:07:28 -0700

    Converted common lib to its own repo.

--- KidesServer/wwwroot/css/Less/compile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call lessc --clean-css generic.less generic.min.css
-call lessc --clean-css music.less music.min.css
-call lessc --clean-css wotMaps.less wotMaps.min.css
-call lessc --clean-css sgDiscord.less sgDiscord.min.css
